3df9d8aa7585930d5878b4374dc09e22fd313f1e
ritwikagrawal1228 <63199820+ritwikagrawal1228@users.noreply.github.com>
2023-05-30 16:24:55 +0530

    Update main.py for CORS

--- main.py
+++ main.py
@@ -17,6 +17,22 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 import psycopg2
